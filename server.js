0ab3dff0b6c8de0a0d923a9b71b1a7778ae2f84e
Neha-Johnson <149090749+Neha-Johnson@users.noreply.github.com>
2023-12-04 20:27:56 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/my_database', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a catch-all route to serve your index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
